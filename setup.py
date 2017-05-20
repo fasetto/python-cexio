4ef89acea2ff035fb9a58912f9d545233a2c2047
SERKAN BIRCAN <serkan@siniradam.net>
2017-05-20 10:05:51 +0300

    Setup added.

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='python-cexio',
+    version='0.1.2',
+    packages=['cexio'],
+    description='Python wrapper for CEX.IO',
+    url='https://github.com/fasetto/python-cexio',
+    author='SERKAN BIRCAN',
+    author_email='serkan@siniradam.net',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Office/Business :: Financial',
+    ]
+)
