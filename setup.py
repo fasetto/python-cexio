3012dae4948b5ee4257b191f2b3e8750c1a15911
SERKAN <serkanbircan21@yandex.com>
2020-08-03 01:00:16 +0300

    fixed an issue with package (#6)
    * fixed setup.py
    
    * updated package version

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='cexio',
-    version='0.1.6',
+    version='0.1.7',
     packages=['cexio'],
     description='Python wrapper for CEX.IO',
     url='https://github.com/fasetto/python-cexio',
     author='fasetto',
     author_email='serkanbircan21@yandex.com',
     license='MIT',
+    include_package_data=True,
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
